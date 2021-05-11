65857e5dbcadf3713608c7339deb63b064ee7232
volerysven <61782949+volerysven@users.noreply.github.com>
2021-05-11 10:21:20 +0200

    Création de la page "register"
    Création de la page register pour le formulaire

---
+++ Code/view/register.php
@@ -0,0 +1,16 @@
+<?php
+ob_start();
+?>
+
+<section id="" class="about-us">
+			
+									<h2>
+										TEST
+									</h2>
+									
+</section>
+
+<?php
+$content = ob_get_clean();
+require "gabarit.php";
+?>
